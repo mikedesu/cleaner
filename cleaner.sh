bca0aa9d364d05ccbfb7835f6c8c3fe5836f449c
darkmage <165251+mikedesu@users.noreply.github.com>
2023-11-17 17:19:32 -0600

    writing files to tmp directory and sending notifications at end of each step

--- cleaner.sh
+++ cleaner.sh
@@ -19,25 +19,31 @@ fi
 
 figlet -f slant cleaner.sh;
 echo "- by darkmage";
-echo "- www.evildojo.com";
+echo "- https://www.evildojo.com";
 echo "- @evildojo666";
 echo "- https://www.github.com/mikedesu/cleaner";
+echo "";
+
+
 
 echo "$(date) Grabbing ports...";
-sed 's|.*:||' $1 | sort > ports.txt;
-uniq -c ports.txt | sort -nr | awk '{print $2}' > just-ports.txt;
-for port in $(head -n $2 just-ports.txt); do
+sed 's|.*:||' $1 | sort > tmp/ports.txt;
+uniq -c tmp/ports.txt | sort -nr | awk '{print $2}' > tmp/just-ports.txt;
+for port in $(head -n $2 tmp/just-ports.txt); do
     echo "$(date) \033[33mScanning\033[0m port $port";
-    ip_list="ip-list-$port.txt"; 
-    grep $port proxies.txt | sed "s/:$port//g" > $ip_list;
-    nmap -vv -iL $ip_list --script socks-open-proxy -p$port -Pn -oG nmap-$port.gnmap;
-    grep open nmap-$port.gnmap | awk '{print $2}' | sed "s/$/:$port/g" > socks-$port.txt;
+    ip_list="tmp/ip-list-$port.txt"; 
+    grep ":$port$" proxies.txt | sed "s/:$port//g" > $ip_list;
+    nmap -vv -iL $ip_list --script socks-open-proxy -p$port -Pn -oG tmp/nmap-$port.gnmap;
+    grep open tmp/nmap-$port.gnmap | awk '{print $2}' | sed "s/$/:$port/g" > tmp/socks-$port.txt;
+    echo "Completed NMAP of port $port" | notify -silent;
 done
-cat socks-*.txt > socks.txt;
+cat tmp/socks-*.txt > tmp/socks.txt;
 
 # for better results, run this command multiple times
 for i in $(seq 1 $3); do
-    parallel --bar -j8 -a socks.txt --round-robin --shuf 'curl -s -x socks5://{} http://www.google.com -o /dev/null -m 10; [[ $? -eq 0 ]] && (echo socks5://{} >> socks-success.txt)';
-    sort -u socks-success.txt -o socks-success.txt;
+    parallel --bar -j16 -a tmp/socks.txt --round-robin --shuf 'curl -s -x socks4://{} http://www.google.com -o /dev/null -m 10; [[ $? -eq 0 ]] && (echo socks4://{} >> success.txt)';
+    parallel --bar -j16 -a tmp/socks.txt --round-robin --shuf 'curl -s -x socks5://{} http://www.google.com -o /dev/null -m 10; [[ $? -eq 0 ]] && (echo socks5://{} >> success.txt)';
+    sort -u success.txt -o success.txt;
+    echo "Total SOCKS proxies found: $(wc -l success.txt | awk '{print $1}')" | notify -silent;
 done
 
